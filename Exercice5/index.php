24e7f7690fa2f1202f27a8c88c8f2aeb5de16980
Pierre <monvoisin.pierre@gmail.com>
2019-12-05 14:26:18 +0100

    Exercice terminé avec l'html et le bootstrap

--- Exercice5/index.php
+++ Exercice5/index.php
@@ -1,7 +1,33 @@
-<?php
-  $int;
-  echo $int;
-  $int = 123;
-  echo ' ';
-  echo $int;
-?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <title>Exercice 5</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+
+<body class='container-fluid bg-secondary'>
+
+  <div class="row mt-5">
+    <div class="jumbotron shadow-lg mx-auto text-center">
+      <p>
+        <?php
+          $int;
+          echo $int;
+          $int = 123;
+          echo ' ';
+          echo $int;
+        ?>
+      </p>
+      <div class="text-right mt-1">Un message de la direction</div>
+    </div>
+  </div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="assets/js/script.js"></script>
+</body>
+</html>
--- Exercice8/index.php
+++ Exercice8/index.php
@@ -1,7 +1,33 @@
-<?php
-  $addition = 3 + 4;
-  $multiplication = 5 * 20;
-  $division = 45 / 5;
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <title>Exercice 8</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
 
-  echo "3 + 4 = $addition, 5 * 20 = $multiplication, 45 / 5 = $division";
-?>
+<body class='container-fluid bg-secondary'>
+
+  <div class="row mt-5">
+    <div class="jumbotron shadow-lg mx-auto text-center">
+      <p>
+        <?php
+          $addition = 3 + 4;
+          $multiplication = 5 * 20;
+          $division = 45 / 5;
+
+          echo "3 + 4 = $addition, 5 * 20 = $multiplication, 45 / 5 = $division";
+        ?>
+      </p>
+      <div class="text-right mt-1">Un message de la direction</div>
+    </div>
+  </div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="assets/js/script.js"></script>
+</body>
+</html>
